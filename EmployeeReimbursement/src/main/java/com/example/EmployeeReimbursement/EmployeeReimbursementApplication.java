5caecd8a04893d5d56f5563a4124c342cb068d4c
Jingxun Yin <jingxun.yin0513@outlook.com>
2024-12-14 14:42:10 -0500

    initial commit

---
+++ EmployeeReimbursement/src/main/java/com/example/EmployeeReimbursement/EmployeeReimbursementApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeReimbursement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeReimbursementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeReimbursementApplication.class, args);
+	}
+
+}
---
+++ EmployeeReimbursement/src/test/java/com/example/EmployeeReimbursement/EmployeeReimbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeReimbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeReimbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
